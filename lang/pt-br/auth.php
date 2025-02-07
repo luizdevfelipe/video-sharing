be37a2362bc3d3678e8add6e80f524e3c63cf314
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-02-07 16:59:43 -0300

    arquivos de tradução

---
+++ lang/pt-br/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Email e/ou senha incorreto(s).',
+    'password' => 'A senha informada está incorreta.',
+    'throttle' => 'Muitas tentativas de LogIn. Tente novamente em :seconds segundos.',
+    'emails.action_subcopy' => 'Se você está tendo problemas para clicar no botão \":actionText\", copie e cole o URL abaixo em seu navegador:',
+];
