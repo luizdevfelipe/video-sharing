4da1f6b8d96b8fc9e214f43811d354e77656399a
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-02-10 16:45:24 -0300

    página principal do site, com componentes testes

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,9 @@
+<x-layouts.main title="Video Sharing">
+    <div class="grid justify-items-center grid-cols-1 gap-2 p-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <x-cards.video></x-cards.video>
+        <x-cards.video></x-cards.video>
+        <x-cards.video></x-cards.video>
+        <x-cards.video></x-cards.video>
+        <x-cards.video></x-cards.video>
+    </div>    
+</x-layouts.main>
