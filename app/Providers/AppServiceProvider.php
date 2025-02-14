78501201eca2b82ff939e98f5c307a70356c7b9f
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-02-14 17:04:42 -0300

    Limite de pedidos nas rotas de autenticação

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\Passport;
 
@@ -23,5 +26,12 @@ class AppServiceProvider extends ServiceProvider
         Passport::tokensExpireIn(now()->addDays(15));
         Passport::refreshTokensExpireIn(now()->addDays(30));
         Passport::personalAccessTokensExpireIn(now()->addMonths(6));
+
+        RateLimiter::for('login', function (Request $request) {
+            return [
+                Limit::perMinute(5),
+                Limit::perMinute(3)->by($request->input('email')),
+            ];
+        });
     }
 }
