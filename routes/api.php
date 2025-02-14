7846322ed5921e8eedb95d7e5ce82a3b8d995351
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-02-14 17:05:14 -0300

    rotas de verificação de credenciais para autenticação

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+});
+
+Route::middleware(['auth:api', 'throttle:login'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return response()->json($request->user());
+    });
+});
