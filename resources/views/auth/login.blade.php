751dccdc8e2896a3ebabecff42b8d461e24baf23
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-02-19 17:11:08 -0300

    Rota para login com Google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
     </div>
 
     <div class="flex relative items-center justify-center mb-5">
-        <a href="" class="bg-gray-100 rounded-sm p-1 hover:shadow-lg dark:bg-gray-700">{{ __('auth.google-login') }}</a>       
+        <a href="{{ route('auth.google') }}" class="bg-gray-100 rounded-sm p-1 hover:shadow-lg dark:bg-gray-700">{{ __('auth.google-login') }}</a>       
     </div>
 
     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{{ __('Submit') }}</button>
