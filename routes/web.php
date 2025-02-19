0e0dc3017e5df47dd4ff372d375f302b832bc4c0
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-02-19 17:11:31 -0300

    Rotas de login e autenticação do usuário com Google

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GoogleAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,6 +10,11 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
+
+Route::controller(GoogleAuthController::class)->group(function () {
+    Route::get('/auth/google', 'logIn')->name('auth.google');
+    Route::get('/auth/google-callback', 'authenticate')->name('auth.authenticate');
+});
 
 Route::get('/register', function () {
     return view('auth.register');
