b4c20c56ecb390185087b09b12f87cbb797427d9
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-02-24 17:11:55 -0300

    Página de perfil
    Página de perfil com base no layout principal e que utiliza uma seção experimental

---
+++ resources/views/profile/profile.blade.php
@@ -0,0 +1,4 @@
+<x-layouts.main title="Video Sharing">
+    <x-sections.profile-section name="Histórico" :sources="['resources/images/image.png', 'resources/images/image.png']">
+    </x-sections.profile-section>
+</x-layouts.main>
