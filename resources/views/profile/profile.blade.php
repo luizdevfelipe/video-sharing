7d25214a112531e36332f7d2d376b0fc25c0efa0
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-02-25 17:07:45 -0300

    adição do modal em forma de component

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -1,4 +1,15 @@
 <x-layouts.main title="Video Sharing">
-    <x-sections.profile-section name="Histórico" :sources="['resources/images/image.png', 'resources/images/image.png']">
+    <x-sections.profile-section name="{{ __('History') }}" :sources="['video1' => 'resources/images/image.png', 'video2' => 'resources/images/image.png']">
     </x-sections.profile-section>
+
+    <x-sections.profile-section name="Playlists" :sources="['Pl1' => 'resources/images/image.png', 'Pl2' => 'resources/images/image.png']">
+    </x-sections.profile-section>
+
+    
+
+    <x-modals.upload-video text="+" class="absolute z-1 left-1/2 bottom-10 -translate-x-1/2">
+    </x-modals.upload-video>
+
+
+
 </x-layouts.main>
