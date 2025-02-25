ce37742bf468ae0f3550efbe639d952ae1fc53b4
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-02-25 17:07:27 -0300

    nomes dos vídeos carregados dinâmicamente

--- resources/views/components/sections/profile-section.blade.php
+++ resources/views/components/sections/profile-section.blade.php
@@ -2,9 +2,9 @@
     <p class="mb-2">{{ $name }}</p>
     <div>
         <ul class="flex flex-wrap justify-start gap-1">
-            @foreach ($sources as $source)
+            @foreach ($sources as $name => $source)
             <li class="p-1 rounded-sm bg-gray-100 dark:bg-gray-600"><img class="max-w-40" src="{{ Vite::asset($source) }}" alt="">
-                <p>Video 1</p>
+                <p>{{ $name }}</p>
             </li>
             @endforeach
             <li class="self-end"><a href="#" class="text-blue-400">{{ __('See More') }}</a></li>
