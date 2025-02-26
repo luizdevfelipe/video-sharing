b7e75cb90060018ba10deb8ec748974abd04aa3a
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-02-26 16:58:36 -0300

    Opacidade no fundo, menu de categoria, organização
    Adicionado efeito de escurecimento do fundo da tela quando o modal é exibido; um menu dropdown para seleção das categorias do vídeo; organização geral do documento com correção na localização do elemento de envio de arquivos

--- resources/views/components/modals/upload-video.blade.php
+++ resources/views/components/modals/upload-video.blade.php
@@ -1,10 +1,10 @@
 {{-- Modal toggle --}}
-<button data-modal-target="crud-modal" data-modal-toggle="crud-modal" {{ $attributes->merge( ['class' => "block pb-1 px-3 size-16 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-[50%] text-[1.4em] text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"] ) }} type="button">
+<button id="toggleUploadVideoModal" data-modal-target="crud-modal" data-modal-toggle="crud-modal" {{ $attributes->merge( ['class' => "block pb-1 px-3 size-16 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-[50%] text-[1.4em] text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"] ) }} type="button">
     {{ $text }}
 </button>
 
 {{-- Main modal --}}
-<div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-white/50 dark:bg-gray-800/50">
     <div class="relative p-4 w-full max-w-md max-h-full">
         {{-- Modal content --}}
         <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
@@ -21,38 +21,98 @@
                 </button>
             </div>
             {{-- Modal body --}}
-            <form class="p-4 md:p-5">
+            <form method="POST" action="{{ route('profile.video') }}" class="p-4 md:p-5">
+                @csrf
                 <div class="grid gap-4 mb-4 grid-cols-2">
+
+                    {{-- Title --}}
                     <div class="col-span-2">
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __("Title") }}</label>
                         <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="{{ __('Title must contain a maximum of 255 characters') }}" required minlength="10" maxlength="255">
                     </div>
-
-
+                    
+                    {{-- Description --}}
                     <div class="col-span-2">
                         <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __("Description") }}</label>
                         <textarea id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="{{ __('Description must contain a maximum of 3,000 characters') }}"></textarea minlength="200" maxlength="3000" required>
                     </div>
-                </div>
 
+                    {{-- Categories --}}
+                    <div class="col-span-2">                       
+                        <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                            {{  __("Select a category") }}
+                            <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/></svg>
+                        </button>
 
-                <div class="col-span-2">
-                    <div class="flex items-center justify-center w-full">
-                        <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                            <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                                <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                                </svg>
-                                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                                <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                            </div>
-                            <input id="dropzone-file" type="file" class="hidden" />
-                        </label>
+                        {{-- Categories menu --}}
+                        <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700">
+                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                            <li>
+                                <div class="flex">
+                                    <div class="flex items-center h-5">
+                                        <input id="helper-checkbox" aria-describedby="helper-checkbox-text" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                    </div>
+                                    <div class="ms-2 text-sm">
+                                        <label for="helper-checkbox" class="font-medium text-gray-900 dark:text-gray-300">Lorem ipsum dolor sit amet.</label>
+                                        <p id="helper-checkbox-text" class="text-xs font-normal text-gray-500 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, accusantium!</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex">
+                                    <div class="flex items-center h-5">
+                                        <input id="helper-checkbox" aria-describedby="helper-checkbox-text" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                    </div>
+                                    <div class="ms-2 text-sm">
+                                        <label for="helper-checkbox" class="font-medium text-gray-900 dark:text-gray-300">Lorem ipsum dolor sit amet.</label>
+                                        <p id="helper-checkbox-text" class="text-xs font-normal text-gray-500 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, accusantium!</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex">
+                                    <div class="flex items-center h-5">
+                                        <input id="helper-checkbox" aria-describedby="helper-checkbox-text" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                    </div>
+                                    <div class="ms-2 text-sm">
+                                        <label for="helper-checkbox" class="font-medium text-gray-900 dark:text-gray-300">Lorem ipsum dolor sit amet.</label>
+                                        <p id="helper-checkbox-text" class="text-xs font-normal text-gray-500 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, accusantium!</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex">
+                                    <div class="flex items-center h-5">
+                                        <input id="helper-checkbox" aria-describedby="helper-checkbox-text" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                    </div>
+                                    <div class="ms-2 text-sm">
+                                        <label for="helper-checkbox" class="font-medium text-gray-900 dark:text-gray-300">Lorem ipsum dolor sit amet.</label>
+                                        <p id="helper-checkbox-text" class="text-xs font-normal text-gray-500 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, accusantium!</p>
+                                    </div>
+                                </div>
+                            </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
 
+                    {{-- Video --}}
+                    <div class="col-span-2">
+                        <div class="flex items-center justify-center w-full">
+                            <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                                    <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                                    </svg>
+                                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                                    <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                </div>
+                                <input id="dropzone-file" type="file" class="hidden" />
+                            </label>
+                        </div>
+                    </div>
+                </div>                
 
-                <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm mt-1 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
                     </svg>
