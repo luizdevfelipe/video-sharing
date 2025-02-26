cec0e1153017740e02aa7ba98bd46e3948c00ec6
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-02-26 16:56:08 -0300

    Update profile.blade.php

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -5,11 +5,6 @@
     <x-sections.profile-section name="Playlists" :sources="['Pl1' => 'resources/images/image.png', 'Pl2' => 'resources/images/image.png']">
     </x-sections.profile-section>
 
-    
-
     <x-modals.upload-video text="+" class="absolute z-1 left-1/2 bottom-10 -translate-x-1/2">
     </x-modals.upload-video>
-
-
-
 </x-layouts.main>
