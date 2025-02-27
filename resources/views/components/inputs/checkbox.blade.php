fcd62e48b07e753296954369308b56ab7549fd9b
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-02-27 17:00:14 -0300

    componente para input:checkbox

---
+++ resources/views/components/inputs/checkbox.blade.php
@@ -0,0 +1,9 @@
+<div class="flex">
+    <div class="flex items-center h-5">
+        <input id="{{ $name }}" name="{{ $name }}" aria-describedby="helper-checkbox-text" type="checkbox" value="{{ $value ?? null }}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+    </div>
+    <div class="ms-2 text-sm">
+        <label for="{{ $name }}" class="font-medium text-gray-900 dark:text-gray-300">{{ $text }}</label>
+        <p id="{{ $name }}-description-text" class="text-xs font-normal text-gray-500 dark:text-gray-300">{{ $description ?? null }}</p>
+    </div>
+</div>
