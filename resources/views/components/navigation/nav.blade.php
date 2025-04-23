eedf5f446983ad6f875263d8b69b1146ff9df9a4
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-04-23 16:07:14 -0300

    rotas de gerenciamento da conta e vídeos
    Páginas a serem implementadas para gerenciamento da segurança e dados da conta além da página de perfil exposta para as pessoas contendo os vídeos publicados por um usuário

--- resources/views/components/navigation/nav.blade.php
+++ resources/views/components/navigation/nav.blade.php
@@ -32,9 +32,18 @@
   {{-- Dropdown Menu --}}
   <div id="dropdownDivider" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600">
     <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDividerButton">
+      @if ( request()->routeIs('profile.index') )
+      <li>
+        <a href="{{ route('profile.settings') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ __('Manage Account') }}</a>
+      </li>
+      <li>
+        <a href="{{ route('profile.edit-videos') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ __('Manage your videos') }}</a>
+      </li>
+      @else
       <li>
         <a href="{{ route('profile.index') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ __('Your Profile') }}</a>
       </li>
+      @endif
     </ul>
     <div>
       <form action="{{ route('logout') }}" method="post" class="block text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
