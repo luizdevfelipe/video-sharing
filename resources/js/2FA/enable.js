99c4600e996e52c54b4b92bbca79a0ff79926cd3
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-04-30 22:08:36 -0300

    controle de requisições via AJAX para ativar 2FA

--- resources/js/2FA/enable.js
+++ resources/js/2FA/enable.js
@@ -1,18 +1,26 @@
-$(document).ready(function() {
-    $('#twoFactorForm').submit(function(e) {
+import { getRoutes } from '@/js/routes.js';
+
+var routes = getRoutes();
+
+$(document).ready(function () {
+    $('#twoFactorForm').submit(function (e) {
         e.preventDefault();
 
         const form = $(this);
         const url = form.attr('action');
 
-        $.post(url).done(
-            function() {
-                $.get('/user/two-factor-qr-code', function(response) {
-                    $('#twoFactorForm').html(response.svg);
-                });
-            }
-        ).fail(function(error) {
-            console.error('Erro ao ativar 2FA:', error);
+        $.post(url)
+        .done(function () {
+            activateTwoFactor();
+        }).fail(function (error) {
+           window.location.href = routes['password.confirm'];
         });
+
+        function activateTwoFactor() {
+            $.get(routes['two-factor.qr-code'], function (response) {
+                $('#twoFactorForm').html(response.svg);
+                $('#twoFactorForm').append('<p class="text-center dark:text-white">Scan the QR code with your authenticator app.</p>');
+            });
+        }
     });
 });
