d2df986e3ad555ff69069156071b9b4d8c5e47de
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-05-08 16:20:02 -0300

    cor branca de fundo do QR
    cor de fundo da div que contém o QR alterada, pois não estava sendo possível lê-lo devido ao fundo escuro do respectivo tema

--- resources/js/2FA/enable.js
+++ resources/js/2FA/enable.js
@@ -28,10 +28,10 @@ $(document).ready(function () {
 
         function activateTwoFactor() {
             $.get(routes['two-factor.qr-code'], function (response) {
-                $('#twoFactorForm').html(`<div class="flex justify-center">${response.svg}</div>`);
+                $('#twoFactorForm').html(`<div class="m-auto w-55"><div class="flex justify-center bg-white p-2">${response.svg}</div></div>`);
                 $('#twoFactorForm').attr('action', routes['two-factor.confirm']);
                 $('#twoFactorForm').append(`<p class="text-center dark:text-white">${translations.enable2FACode}</p>`);
-                $('#twoFactorForm').append(`<div class="mb-5 size-64 m-auto h-20"><label for="iconfirmCode" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">${translations.code}</label><input type="text" id="iconfirmCode" name="confirmCode" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123456" required autofocus /></div>`);
+                $('#twoFactorForm').append(`<div class="mb-5 size-64 m-auto h-20"><label for="icode" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">${translations.code}</label><input type="text" id="icode" name="code" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123456" required autofocus /></div>`);
 
                 $('#twoFactorForm').append(`<div class="flex justify-center"><button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer">${translations.submit}</button></div>`);
             });
