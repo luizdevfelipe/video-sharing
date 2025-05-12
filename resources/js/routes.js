c98e9e2f6cb1759d7cc433aa1d67b208192c9762
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-05-12 16:04:17 -0300

    Update routes.js

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -3,6 +3,8 @@ export function getRoutes() {
         'password.confirm': '/user/confirm-password',
         'two-factor.qr-code': '/user/two-factor-qr-code',
         'two-factor.confirm': '/user/confirmed-two-factor-authentication',
+        'profile.settings' : '/profile/settings',
+        'two-factor.recovery-codes': '/user/two-factor-recovery-codes'
     }
 
 }
