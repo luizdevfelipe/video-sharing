23fc9d6d8564cadcd8c1901d02459444c13d4144
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-05-13 16:19:55 -0300

    id baseado no name e propriedade autofocus

--- resources/views/components/inputs/text.blade.php
+++ resources/views/components/inputs/text.blade.php
@@ -1,4 +1,4 @@
 <div class="mb-5">
-    <label for="{{ $name }}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __($label) }}</label>
-    <input type="{{ $type ?? 'text' }}" id="{{ $name }}" name="{{ $name }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="{{ __($placeHolder ?? null) }}" required {{ $htmlAtributes ?? null }} />
+    <label for="i{{ $name }}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __($label) }}</label>
+    <input type="{{ $type ?? 'text' }}" id="i{{ $name }}" name="{{ $name }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="{{ __($placeHolder ?? null) }}" required {{ $htmlAtributes ?? null }} autofocus/>
 </div>
