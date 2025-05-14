e1723638cf5b6f04b34b075194a0803effc47456
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-05-14 16:11:36 -0300

    correção
    Adicionado as chaves que representam um trecho que executa código php para a função de carregamento de tradução

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.auth title="{{ __('auth.confirm-password') }}">
   <form id="loginForm" action="{{ route('password.confirm.store') }}" method="POST">
-    <legend class="text-center text-xl font-bold">__('auth.confirm-password')</legend>
+    <legend class="text-center text-xl font-bold">{{__('auth.confirm-password')}}</legend>
     @csrf
     <x-inputs.text name="password" type="password" label="auth.pass-label" />
 
