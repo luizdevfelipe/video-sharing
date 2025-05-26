9088c32bb2a54bc078a356930abba5b21e7d5a6d
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-05-26 16:13:18 -0300

    traduções para campos de formulários

--- lang/pt-br/validation.php
+++ lang/pt-br/validation.php
@@ -195,6 +195,11 @@ return [
         'pfoto' => 'Foto',
         'password' => 'Senha',
         'preco' => 'Preço',
+        'categories' => 'Categorias',
+        'title' => 'Título',
+        'description' => 'Descrição',
+        'video' => 'Vídeo',
+        'thumbnail' => 'Miniatura',
     ],
 
 ];
