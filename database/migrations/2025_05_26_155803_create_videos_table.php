d9beea6bc8318c0a0b0768dc3c21a50797f9b784
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-05-27 16:08:51 -0300

    Create 2025_05_26_155803_create_videos_table.php

---
+++ database/migrations/2025_05_26_155803_create_videos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * 
+     */
+    public function up(): void
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('description', 3000);
+            $table->string('video_path');
+            $table->string('thumbnail_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('videos');
+    }
+};
