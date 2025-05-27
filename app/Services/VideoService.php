741b19c85ec70b42a131ee22b837cc441dc762c5
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-05-27 16:08:45 -0300

    Create VideoService.php

---
+++ app/Services/VideoService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+class VideoService
+{
+    /**
+     * Create a new video entry in the database.
+     *
+     * @param string $title
+     * @param string $description
+     * @param array $categories
+     * @param string $videoPath
+     * @param string $thumbnailPath
+     * @return void
+     */
+    public function createVideo(
+        string $title, string $description, array $categories, string $videoPath, string $thumbnailPath
+    ): void
+    {
+         
+    }
+}
