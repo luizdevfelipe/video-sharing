c4ecd1dacbc3f1d91798291a82d9d3a783dbd716
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-05-30 17:20:52 -0300

    Armazenamento dos dados do vídeo no banco

--- app/Services/VideoService.php
+++ app/Services/VideoService.php
@@ -2,6 +2,9 @@
 
 namespace App\Services;
 
+use App\Models\Category;
+use App\Models\Video;
+
 class VideoService
 {
     /**
@@ -15,9 +18,26 @@ class VideoService
      * @return void
      */
     public function createVideo(
-        string $title, string $description, array $categories, string $videoPath, string $thumbnailPath
-    ): void
-    {
-         
+        string $title,
+        string $description,
+        array $categories,
+        string $videoPath,
+        string $thumbnailPath
+    ): void {
+        $video = Video::create([
+            'title' => $title,
+            'description' => $description,
+            'video_path' => $videoPath,
+            'thumbnail_path' => $thumbnailPath,
+        ]);
+
+        $categoryIds = array_map(function ($category) {
+            $id = Category::where('name', $category)->first()->id;
+            if ($id) {
+                return $id;
+            }
+        }, $categories);
+
+        $video->categories()->attach($categoryIds);
     }
 }
