7314ac73d778d7da01b92eeeff229525539721d6
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-06-03 17:02:52 -0300

    inicialiação do Vue no ponto de entrada da aplicação

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,19 @@
+// Importing necessary packages and styles
 import './bootstrap';
 import 'flowbite';
 import.meta.glob([
     '../images/**'
 ]);
 import $ from 'jquery';
+import { createApp } from 'vue';
+import App from './components/App.vue';
+
+// Defining global variables
 window.$ = window.jQuery = $;
 $.ajaxSetup({
     headers: {
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
     }
 });
+
+createApp(App).mount('#app');
