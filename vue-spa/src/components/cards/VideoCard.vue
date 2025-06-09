f8c3c92d5a222fc466b7acb2fad1c63e96ec8c33
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-06-09 17:00:13 -0300

    Create VideoCard.vue

---
+++ vue-spa/src/components/cards/VideoCard.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bg-gray-200 rounded-t-lg max-w-sm h-80 dark:bg-gray-800">
+        <div>
+            <!-- {{ asset('storage/public/poster/image.png') }} -->
+            <img src="" alt="" class="rounded-t-lg shadow-md dark:shadow-gray-800" style="max-width: 100%; max-height: 100%;" />
+        </div>
+        <p class="text-dark font-bold text-xl dark:text-white">Teste de Vídeo</p>
+    </div>
+</template>
