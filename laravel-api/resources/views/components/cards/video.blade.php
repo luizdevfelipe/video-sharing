e1380865d4205366bd6ba146fac19db421c982ca
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-06-09 16:55:49 -0300

    Delete video.blade.php

--- laravel-api/resources/views/components/cards/video.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="bg-gray-200 rounded-t-lg max-w-sm h-80 dark:bg-gray-800">
-    <div>
-        <img src="{{ asset('storage/public/poster/image.png') }}" alt="" class="rounded-t-lg shadow-md dark:shadow-gray-800" style="max-width: 100%; max-height: 100%;" />
-    </div>
-
-    <p class="text-dark font-bold text-xl dark:text-white">Teste de Vídeo</p>
-    
-</div>
