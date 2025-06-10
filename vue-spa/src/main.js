e9a3c36136634364eac615e65de3ecbcd8799e55
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-06-10 17:18:14 -0300

    definição da linguagem da página

--- vue-spa/src/main.js
+++ vue-spa/src/main.js
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+document.documentElement.lang = import.meta.env.VITE_APP_LOCALE || 'en';
+
 const app = createApp(App)
 
 app.use(router)
