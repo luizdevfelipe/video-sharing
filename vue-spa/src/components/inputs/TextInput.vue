bfcdda4a65366b4c06cfe77fb28876975a23c522
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-06-16 20:50:20 -0300

    configuração para passagem de v-model

--- vue-spa/src/components/inputs/TextInput.vue
+++ vue-spa/src/components/inputs/TextInput.vue
@@ -1,5 +1,6 @@
 <script setup>
 defineProps({
+    modelValue: [String, Number],
     name: String,
     type: [String, null],
     label: String,
@@ -9,11 +10,13 @@ defineProps({
         default: () => ({})
     }
 })
+
+const emit = defineEmits(['update:modelValue'])
 </script>
 
 <template>
     <div class="mb-5">
         <label :for="'i'+name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ label }}</label>
-        <input :type="(type ? type : 'text')" :id="'i'+name" :name="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :placeholder="placeHolder" required v-bind="htmlAttributes"/>
+        <input :type="(type ? type : 'text')" :id="'i'+name" :name="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" @input="emit('update:modelValue', $event.target.value)" :placeholder="placeHolder" required v-bind="htmlAttributes"/>
     </div>
 </template>
