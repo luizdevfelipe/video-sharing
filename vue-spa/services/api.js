fc61699954d248103533b4459301033da22feaab
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-06-16 20:48:13 -0300

    Padronização do domínio
    Para que a autenticação SPA via Sanctum funcione o domínio usado na aplicação como um todo deve ser o mesmo

--- vue-spa/services/api.js
+++ vue-spa/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:80/api',
+  baseURL: 'http://127.0.0.1:80',
+  withCredentials: true,
+  withXSRFToken: true
 })
 
 export default api
