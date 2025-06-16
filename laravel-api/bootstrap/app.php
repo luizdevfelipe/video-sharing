66565417ff6366d7fe5797256bc4b8da7a9a0bc4
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-06-16 20:49:42 -0300

    middleware de autenticação Sanctum

--- laravel-api/bootstrap/app.php
+++ laravel-api/bootstrap/app.php
@@ -22,6 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'password.confirm' => CustomRequirePassword::class
         ]);
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
