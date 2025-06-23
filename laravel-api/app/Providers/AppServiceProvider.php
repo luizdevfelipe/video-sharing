09287d73c457f076ae6b4c6aa151b9bef2078f00
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-06-23 16:25:56 -0300

    remoção do Laravel Passport
    Essa biblioteca de autenticação e gerenciamento de APIs não é mais necessário, pois a aplicação passou a fazer o uso do Laravel Sanctum que é mais simples e adequado

--- laravel-api/app/Providers/AppServiceProvider.php
+++ laravel-api/app/Providers/AppServiceProvider.php
@@ -6,7 +6,6 @@ use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,10 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Passport::tokensExpireIn(now()->addDays(15));
-        Passport::refreshTokensExpireIn(now()->addDays(30));
-        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
-
         RateLimiter::for('login', function (Request $request) {
             return [
                 Limit::perMinute(5),
