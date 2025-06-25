73dc83fa33d2438c1dea65a14650377bd6510fb8
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-06-24 22:44:39 -0300

    remoção das visualizações e features
    Isso é necessário para que apenas as rotas "lógicas" realmente necessárias sejam disponibilzadas para serem consumidas pela SPA Vue.JS

--- laravel-api/config/fortify.php
+++ laravel-api/config/fortify.php
@@ -130,7 +130,7 @@ return [
     |
     */
 
-    'views' => true,
+    'views' => false,
 
     /*
     |--------------------------------------------------------------------------
@@ -143,17 +143,6 @@ return [
     |
     */
 
-    'features' => [
-        // Features::registration(),
-        Features::resetPasswords(),
-        Features::emailVerification(),
-        // Features::updateProfileInformation(),
-        // Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirm' => true,
-            'confirmPassword' => true,
-            'window' => 0,
-        ]),
-    ],
+    'features' => [],
 
 ];
