19027e422ebb4d9d73d5adb16c68ea3863a5f120
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-07-09 22:22:22 -0300

    texto informativo passado via props

--- vue-spa/src/components/inputs/Dropfile.vue
+++ vue-spa/src/components/inputs/Dropfile.vue
@@ -2,7 +2,8 @@
 import { getTranslations } from '@/assets/js/translations';
 defineProps({
     modelValue: File,
-    name: String
+    name: String,
+    fileTypes: String
 });
 
 const emit = defineEmits(['update:modelValue']);
@@ -22,7 +23,7 @@ const translations = getTranslations()
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
                     </svg>
                     <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">{{ translations.clickToUpload }} </span> {{ translations.orDragAndDrop }}</p>
-                    <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                    <p class="text-xs text-gray-500 dark:text-gray-400">{{ fileTypes }}</p>
                 </div>
                 <input :id="'i'+name" :name="name" type="file" class="hidden" @input="onInput" />
             </label>
