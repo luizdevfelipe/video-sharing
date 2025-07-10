870fe31ccfe16f82a5da8c1093e3a5d9592c7933
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-07-09 22:18:47 -0300

    campos de título e linguagem para traduções

--- laravel-api/database/migrations/2025_05_27_154048_create_categories_table.php
+++ laravel-api/database/migrations/2025_05_27_154048_create_categories_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50)->unique();
+            $table->string('name', 50);
+            $table->string('title', 50);
             $table->string('description', 255);
+            $table->string('lang', 10)->default('en');
             $table->timestamps();
         });
     }
