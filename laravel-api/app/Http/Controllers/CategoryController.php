e90e7f0e1135cb2f9c44c3cd90b54788e48bdc41
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-07-09 22:15:22 -0300

    retorno de categorias traduzidas, mas com mesmo name

--- laravel-api/app/Http/Controllers/CategoryController.php
+++ laravel-api/app/Http/Controllers/CategoryController.php
@@ -9,15 +9,7 @@ class CategoryController extends Controller
 {
     public function getCategories(): JsonResponse
     {
-        $categories = Category::select('name', 'description')->orderBy('name')->get()->toArray();
-
-        if (config('app.locale') !== 'en') {
-            $translatedReturn = array_map(function ($category) {
-                return ['name' => __('categories.' . $category['name'] . '.name'), 'description' => __('categories.' . $category['name'] . '.description')];
-            }, $categories);
-
-            return response()->json($translatedReturn);
-        }
+        $categories = Category::select('name', 'title', 'description')->where('lang', config('app.locale'))->orderBy('name')->get()->toArray();
 
         return response()->json($categories);
     }
