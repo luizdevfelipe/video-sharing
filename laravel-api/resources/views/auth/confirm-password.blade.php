6a0dcc8dedbe104aade08e66f1334cdae9055332
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-07-11 16:53:33 -0300

    Views e Components não utilizados

--- laravel-api/resources/views/auth/confirm-password.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layouts.auth title="{{ __('auth.confirm-password') }}">
-  <form id="loginForm" action="{{ route('password.confirm.store') }}" method="POST">
-    <legend class="text-center text-xl font-bold">{{__('auth.confirm-password')}}</legend>
-    @csrf
-    <x-inputs.text name="password" type="password" label="auth.pass-label" />
-
-    <x-inputs.submit text="Submit" />
-  </form>
-</x-layouts.auth>
