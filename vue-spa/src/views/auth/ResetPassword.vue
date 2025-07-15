1ff34e38f038987deda3c950dd312f6ef68442e3
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-07-14 23:14:04 -0300

    Página de envio de nova senha

--- vue-spa/src/views/auth/ResetPassword.vue
+++ vue-spa/src/views/auth/ResetPassword.vue
@@ -1,26 +1,52 @@
 <script setup>
-
 import TextInput from '@/components/inputs/TextInput.vue';
 import AuthLayout from '../layouts/AuthLayout.vue';
+import Submit from '@/components/inputs/Submit.vue';
+import api from '../../../services/api';
 import { getTranslations } from '@/assets/js/translations.js';
+import { useRoute } from 'vue-router';
+import { ref } from 'vue';
 
-// request()->route('token')
+const route = useRoute()
 
+const token = route.params.token;
+const email = ref('');
+const password = ref('');
+const password_confirmation = ref('');
+const errors = ref(null);
+
+function submitForm() {
+    api.post('/api/reset-password', {
+        token: token,
+        email: email.value,
+        password: password.value,
+        password_confirmation: password_confirmation.value
+    },
+    ).then(response => {
+        alert(response.data.message);
+        window.location.href = '/login';
+    }).catch(error => {
+        errors.value = error.response.data.message;
+    })
+}
 const translations = getTranslations();
 </script>
 
 <template>
     <AuthLayout :pageTitle="translations.authResetPassword">
-        <form id="registerForm" method="POST">
+        <form id="registerForm" @submit.prevent="submitForm" method="POST">
             <TextInput name="email" type="email" :label="translations.authEmailLabel"
                 :placeHolder="translations.authEmailPlaceholder" v-model="email" />
 
-            <TextInput name="password" type="password" :label="translations.authPassLabel"
-                v-model="password" />
+            <TextInput name="password" type="password" :label="translations.authPassLabel" v-model="password" />
 
-            <TextInput type="password" name="password_confirmation" :label="translations.authPassConfirmLabel" v-model="password_confirmation" />
+            <TextInput type="password" name="password_confirmation" :label="translations.authPassConfirmLabel"
+                v-model="password_confirmation" />
 
             <Submit :text="translations.authSubmit" />
         </form>
+        <template #errors>
+            <p v-if="errors !== null">{{ errors }}</p>
+        </template>
     </AuthLayout>
 </template>
