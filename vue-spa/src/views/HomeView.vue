aae7160dc2a01cea2ac4b9105866eb92ae35671c
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-07-21 22:54:09 -0300

    laço de repetição para cada vídeo sendo mostrado

--- vue-spa/src/views/HomeView.vue
+++ vue-spa/src/views/HomeView.vue
@@ -1,17 +1,25 @@
 <script setup>
 import MainLayout from '@/views/layouts/MainLayout.vue';
 import VideoCard from '@/components/cards/VideoCard.vue'
+import { onMounted, ref } from 'vue';
+import api from '../../services/api.js';
+
+const videoData = ref({});
+
+onMounted(() => {
+  api.get('/api/video').then(response => {
+    videoData.value = response.data.data;
+  }).catch(error => {
+    console.log(error);
+  })
+})
+
 </script>
 
 <template>
   <MainLayout>
-    <div class="grid justify-items-center grid-cols-1 gap-2 p-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      <VideoCard/>
-      <VideoCard/>
-      <VideoCard/>
-      <VideoCard/>
-      <VideoCard/>
-      <VideoCard/>
+    <div v-for="video in videoData" class="grid justify-items-center grid-cols-1 gap-2 p-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <VideoCard :id="video.id" :title="video.title" :thumb_file="video.thumbnail_path" />
     </div>
   </MainLayout>
 </template>
