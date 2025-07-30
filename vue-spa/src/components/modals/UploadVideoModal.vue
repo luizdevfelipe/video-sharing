e24f4728f60a4eb7fffd73b2eb70a4d04db9d953
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-07-29 22:58:27 -0300

    modalTarget prop para utilização via toggleModal()

--- vue-spa/src/components/modals/UploadVideoModal.vue
+++ vue-spa/src/components/modals/UploadVideoModal.vue
@@ -1,8 +1,10 @@
 <script setup>
+import { toggleModal } from '@/assets/js/modal';
 import { getTranslations } from '@/assets/js/translations';
 const translations = getTranslations();
 defineProps({
     modalTitle: String,
+    modalTarget: String,
     btText: String,
     classes: [String, null]
 })
@@ -10,12 +12,12 @@ defineProps({
 
 <template>
     <!-- Modal toggle -->
-    <button id="toggleUploadVideoModal" data-modal-target="crud-modal" data-modal-toggle="crud-modal" :class="'block pb-1 px-3 size-16 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-[50%] text-[1.4em] text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 '+classes" type="button">
+    <button id="toggleUploadVideoModal" @click="toggleModal(modalTarget)" :class="'block pb-1 px-3 size-16 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-[50%] text-[1.4em] text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 '+classes" type="button">
         {{ btText }}
     </button>
 
     <!-- Main modal -->
-    <div id="crud-modal" tabindex="-1" aria-hidden="true"
+    <div :id="modalTarget" tabindex="-1" aria-hidden="true"
         class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-white/50 dark:bg-gray-800/50">
         <div class="relative p-4 w-full max-w-md max-h-full">
             <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
@@ -26,8 +28,7 @@ defineProps({
                         {{ modalTitle }}
                     </h3>
                     <button type="button"
-                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                        data-modal-toggle="crud-modal">
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" @click="toggleModal(modalTarget)">
                         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                             viewBox="0 0 14 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
