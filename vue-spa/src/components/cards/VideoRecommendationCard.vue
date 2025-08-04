48dbb590c11118a64ca2f276963d487e51c9feb2
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-08-03 22:32:39 -0300

    ajuste no tamanho do card

--- vue-spa/src/components/cards/VideoRecommendationCard.vue
+++ vue-spa/src/components/cards/VideoRecommendationCard.vue
@@ -5,12 +5,13 @@
 <template>
 
     <a href="#"
-        class="flex items-center bg-white border border-gray-200 rounded-lg shadow-sm flex-row max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <img class="object-cover w-full rounded-t-lg h-96 h-auto w-48 rounded-none rounded-s-lg"
+        class="flex items-center bg-white border border-gray-200 rounded-lg shadow-sm flex-row w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+        >
+        <img class="object-cover rounded-l-lg h-16 w-16"
             src="..." alt="">
-        <div class="flex flex-col justify-between p-4 leading-normal">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Lorem ipsum dolor sit amet.</h5>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem10 -> ipsum dolor sit amet consectetur adipisicing elit. Velit, recusandae?</p>
+        <div class="flex flex-col justify-between p-2 leading-tight w-full">
+            <h5 class="mb-1 text-sm font-semibold tracking-tight text-gray-900 dark:text-white">Lorem ipsum dolor sit amet.</h5>
+            <p class="mb-1 text-xs font-normal text-gray-700 dark:text-gray-400">Lorem10 -> ipsum dolor sit amet consectetur adipisicing elit. Velit, recusandae?</p>
         </div>
     </a>
 
