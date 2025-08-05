b767dcf1fc0ee117a76d47758640404861554160
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-08-04 22:41:51 -0300

    Create AccordionComponent.vue

---
+++ vue-spa/src/components/AccordionComponent.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+    title: String
+});
+</script>
+
+<template>
+
+    <div id="accordion-flush"
+        data-accordion="collapse"
+        data-active-classes="text-gray-900 dark:text-white"
+        data-inactive-classes="text-gray-500 dark:text-gray-400">
+        <h2 id="accordion-flush-heading-1">
+            <button type="button"
+                class="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3"
+                data-accordion-target="#accordion-flush-body-1" aria-expanded="false"
+                aria-controls="accordion-flush-body-1">
+                <span>{{ title }}</span>
+                <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="false"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M9 5 5 1 1 5" />
+                </svg>
+            </button>
+        </h2>
+        <div id="accordion-flush-body-1" class="hidden" aria-labelledby="accordion-flush-heading-1">
+            <slot />
+        </div>
+    </div>
+
+</template>
