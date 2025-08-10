f88d29ba5aab3751df93ac8e5e62011e73420922
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-08-09 21:45:08 -0300

    botão para avançar na paginação

--- vue-spa/src/views/video/Render.vue
+++ vue-spa/src/views/video/Render.vue
@@ -13,27 +13,34 @@ import { user } from '@/stores/user.js';
 const route = useRoute();
 const commentContent = ref("");
 const videoComments = ref([]);
+const videoCommentsPage = ref(1);
+const videoCommentsTotalPages = ref(1);
 
-function getVideoComments() {
-    api.get(`/api/video/${route.params.id}/comment`)
-    .then((response) => {
-        videoComments.value = response.data.data;
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+function getVideoComments(page) {
+    api.get(`/api/video/${route.params.id}/comment?page=${page}`)
+        .then((response) => {
+            if (videoComments.value.length > 0) {
+                videoComments.value.push(...response.data.data);
+            } else {
+                videoComments.value = response.data.data;
+                videoCommentsTotalPages.value = response.data.last_page;
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 }
 
 function postComment() {
     api.post(`/api/video/${route.params.id}/comment`, {
         content: commentContent.value
     })
-    .then((response) => {
-        videoComments.value.unshift(response.data);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+        .then((response) => {
+            videoComments.value.unshift(response.data);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 }
 
 const translations = getTranslations();
@@ -59,7 +66,7 @@ const translations = getTranslations();
             <section id="video-comments"
                 class="p-1 md:order-1 md:[grid-area:-1/1/-1/-1] border-b md:border-b-0 border-gray-300 dark:border-gray-700 md:pl-6 md:pr-6 pb-4 md:pb-0">
 
-                <AccordionComponent @click.once="getVideoComments" :title="translations.comments">
+                <AccordionComponent @click.once="getVideoComments(1)" :title="translations.comments">
                     <form @submit.prevent="postComment" method="post">
                         <textarea
                             class="w-full p-2 mb-2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -74,6 +81,12 @@ const translations = getTranslations();
                     </form>
 
                     <VideoCommentCard v-for="comment in videoComments" :key="comment.id" :comment="comment" />
+
+                    <button v-if="videoCommentsPage < videoCommentsTotalPages"
+                        @click="getVideoComments(++videoCommentsPage)"
+                        class="mt-4 text-blue-600 hover:underline focus:outline-none dark:text-blue-400">
+                        {{ translations.loadMoreComments }}
+                    </button>
 
                 </AccordionComponent>
 
