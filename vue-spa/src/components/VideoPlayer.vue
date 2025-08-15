3a1e368d5c2c2cccb9d2680bcdf07b8f172135e8
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-08-15 17:11:21 -0300

    pedido pelo arquivo do vídeo

--- vue-spa/src/components/VideoPlayer.vue
+++ vue-spa/src/components/VideoPlayer.vue
@@ -1,16 +1,23 @@
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from "vue";
+import { ref, onMounted, onBeforeUnmount, onUpdated, computed } from "vue";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
+
+const back_url = import.meta.env.VITE_APP_BACKEND;
 
 const videoRef = ref(null);
 let player = null;
 
 const props = defineProps({
-    videoPath: {
+    video_file: {
         type: String,
         required: true
     }
+});
+
+const videoPath = computed(() => {
+    if (!props.video_file) return "";
+    return `${back_url}/api/video/${props.video_file}.m3u8`;
 });
 
 onMounted(() => {
@@ -23,6 +30,15 @@ onMounted(() => {
     }
 });
 
+onUpdated(() => {
+    if (player && videoPath.value) {
+        player.src({
+            src: videoPath.value,
+            type: "application/x-mpegURL",
+        });
+    }
+});
+
 onBeforeUnmount(() => {
     if (player) {
         player.dispose();
@@ -31,11 +47,7 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-    <div class="shadow-[0_5px_50px_rgba(0,0,0,0.7)] dark:shadow-[0_5px_50px_rgba(255,255,255,0.16)]">
-        <video ref="videoRef" id="my-player" class="video-js vjs-fluid" controls preload="auto" autoplay
-            data-setup="{}">
-            <source :src="videoPath" type="application/vnd.apple.mpegurl" />
-            <!-- //vjs.zencdn.net/v/oceans.mp4 -->
-        </video>
+    <div>
+        <video ref="videoRef" class="video-js vjs-fluid"></video>
     </div>
 </template>
