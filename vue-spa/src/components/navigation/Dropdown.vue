7b25602e6dd4ea70170203579be0f5ce31ca0aa6
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-08-25 17:17:28 -0300

    dinamicidade no id, permitindo mais de um por página

--- vue-spa/src/components/navigation/Dropdown.vue
+++ vue-spa/src/components/navigation/Dropdown.vue
@@ -1,23 +1,30 @@
 <script setup>
 defineProps({
     btText: String,
-    classes: [String, null]
+    classes: [String, null],
+    id: {
+        type: String,
+        default: 'dropdown'
+    }
 })
 </script>
 
 <template>
-    <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-    {{ btText }}
-        <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+    <button :id="`${id}-button-dropdown`" :data-dropdown-toggle="`${id}-dropdown`"
+        class="text-white mx-1 mb-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        type="button">
+        {{ btText }}
+        <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+            viewBox="0 0 10 6">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m1 1 4 4 4-4" />
         </svg>
     </button>
 
-    <div class="mb-5">
-        <div id="dropdown" :class="'z-10 p-2 hidden bg-white divide-y divide-gray-100 rounded-lg dark:bg-gray-700 ' + classes">
-            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                <slot />
-            </ul>
-        </div>
+    <div :id="`${id}-dropdown`"
+        :class="'z-10 p-2 hidden bg-white divide-y divide-gray-100 rounded-lg dark:bg-gray-700 ' + classes">
+        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" :aria-labelledby="`${id}-button-dropdown`">
+            <slot />
+        </ul>
     </div>
 </template>
