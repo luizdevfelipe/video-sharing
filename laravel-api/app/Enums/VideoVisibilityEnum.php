6e450f2b8e5ce9e96e81b30743c4c61e30dda8f8
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-08-27 16:44:20 -0300

    método para obter o valor do enum por string
    Esse método estático permite acessar o valor dos casos do enum a partir de uma string que representa o "case"

--- laravel-api/app/Enums/VideoVisibilityEnum.php
+++ laravel-api/app/Enums/VideoVisibilityEnum.php
@@ -7,4 +7,14 @@ enum VideoVisibilityEnum: int
     case PUBLIC = 1;
     case PROTECTED = 2;
     case PRIVATE = 3;
+
+    public static function fromStringValue(string $case): int
+    {
+        return match (strtolower($case)) {
+            'public' => self::PUBLIC->value,
+            'protected' => self::PROTECTED->value,
+            'private' => self::PRIVATE->value,
+            default => throw new \InvalidArgumentException(__('auth.invalid_visibility') . ' ' . $case),
+        };
+    }
 }
