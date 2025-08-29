2e782294e8caaa7ecf86eec6b5d8b98e2dd27af4
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-08-29 17:16:12 -0300

    novo storage, agora utilizando Pinia

--- vue-spa/src/stores/user.js
+++ vue-spa/src/stores/user.js
@@ -1,2 +1,28 @@
+import { defineStore } from 'pinia';
 import { ref } from 'vue';
-export const user = ref(null);
+import api from '@/services/api';
+
+export const useUserStore = defineStore('user', () => {
+    const user = ref(null);
+
+    async function getUserData(force = false) {
+        if (!user.value || force) {
+            try {
+                const res = await api.get('/api/user');
+                user.value = res.data;
+            } catch {
+                user.value = null;
+            }
+        }
+        return user.value;
+    }
+
+    async function logout() {
+        const result = await api.post('/api/logout')
+        if (result.status === 204) {
+            user.value = null;
+        }
+    }
+
+    return { user, getUserData, logout };
+});
