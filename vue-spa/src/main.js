60e188874a797e0ec35b6fb49ef2976dcb9baabe
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-08-29 17:16:55 -0300

    inicialização do Pinia

--- vue-spa/src/main.js
+++ vue-spa/src/main.js
@@ -1,13 +1,15 @@
-import './assets/main.css'
-
+import './assets/main.css';
+import { createPinia } from 'pinia';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 document.documentElement.lang = import.meta.env.VITE_APP_LOCALE || 'en';
 
+const pinia = createPinia();
 const app = createApp(App)
 
 app.use(router)
+app.use(pinia);
 
 app.mount('#app')
