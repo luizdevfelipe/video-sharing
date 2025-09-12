15d976ae9ee70954ac0b3d7d2edc004a8b2f2f03
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-09-12 17:31:54 -0300

    Update status.php

--- laravel-api/lang/en/status.php
+++ laravel-api/lang/en/status.php
@@ -4,7 +4,7 @@
  * Status messages for video access control
  */
 return [
-    'video_204' => 'Video not found',
+    'video_404' => 'Video not found',
     'login_required' => 'You must be logged in to access this video',
     'private_video' => 'This video is private',
 ];
