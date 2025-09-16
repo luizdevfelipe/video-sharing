9ef48d9897ba4dd8f2abc8fd57c22321e4261a87
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-09-16 17:25:07 -0300

    teste de acesso a arquivos do vídeo público

--- laravel-api/tests/Feature/Middlewares/ControlsVideoAccessTest.php
+++ laravel-api/tests/Feature/Middlewares/ControlsVideoAccessTest.php
@@ -10,9 +10,17 @@ class ControlsVideoAccessTest extends TestCase
 {
     use RefreshDatabase, VideoTestSetupTrait;
 
-    public function test_if_the_video_file_is_blocked_by_null_user() 
+    public function test_if_public_binary_file_is_accessible() 
     {
-        $testData = $this->setupPrivateVideoTest('test0.ts');
+        $testData = $this->setupPublicVideoTest('test0.ts', 'test0');
+
+        $response = $this->get('/api/video/' . $testData['fileName']);
+        $response->assertStatus(200);
+    }
+
+    public function test_if_the_private_video_file_is_blocked_by_null_user() 
+    {
+        $testData = $this->setupPrivateVideoTest('test0.ts', 'test0');
 
         $response = $this->get('/api/video/' . $testData['fileName']);
         $response->assertStatus(403);
