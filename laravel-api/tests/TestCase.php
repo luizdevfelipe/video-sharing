81876e3f273854f7764d165c8c6cf17194b4ac1f
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-10-14 17:19:51 -0300

    Método personalizado de autenticação com JWT

--- laravel-api/tests/TestCase.php
+++ laravel-api/tests/TestCase.php
@@ -2,9 +2,22 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     *  Authenticate the given user via JWT for testing purposes.
+     */
+    protected function actingAsJwt(User $user): TestCase
+    {
+        $token = JWTAuth::fromUser($user);
+        $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json',
+        ]);
+        return $this;
+    }
 }
