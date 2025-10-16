30fb1b1f5ab87d0e6139a23c16e7b11278be4dec
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-10-16 11:09:39 -0300

    Teste de envio de token válido e inválido para refresh

--- laravel-api/tests/Feature/AuthControllerTest.php
+++ laravel-api/tests/Feature/AuthControllerTest.php
@@ -6,22 +6,33 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use Tymon\JWTAuth\Providers\Auth\Illuminate;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthControllerTest extends TestCase
 {
+    use RefreshDatabase;
     public function test_refresh_valid_jwt(): void
     {
         $user = User::factory()->create();
 
-        $token = $this->getJwtToken($user);
-
-        $response = $this
-            ->withHeaders(['Authorization' => 'Bearer ' . $token])
-            ->post('/api/refresh-token');
-
-        $response->dump();
-
+        $response = $this->actingAsJwt($user)->post('/api/refresh-token');
         $response->assertStatus(200);
+        $response->assertJsonStructure(['token']);
     }
 
+    public function test_refresh_invalid_jwt(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAsJwt($user)->post('/api/refresh-token');
+
+        JWTAuth::invalidate();
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' .  $this->getJwtToken($user),
+        ])->post('/api/refresh-token');
+
+        $response->dump();
+        $response->assertStatus(401);
+    }
+}
