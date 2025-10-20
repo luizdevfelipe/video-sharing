1a0fb32b323e6165ad8c25c26adf471d077ced6e
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-10-20 10:57:56 -0300

    Axios Interceptors para erro 401
    Caso o axios receba um erro de falta de altorização ele irá tentar refazer o pedido, mas com o JWT atualizado, isso é feito apenas como segunda camada de proteção

--- vue-spa/src/services/api.js
+++ vue-spa/src/services/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { refreshToken } from './jwt'
 
 const api = axios.create({
   baseURL: 'http://127.0.0.1:80',
@@ -14,4 +15,29 @@ export function removeAuthToken() {
   delete api.defaults.headers.common['Authorization'];
 }
 
-export default api
+api.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const newToken = await refreshToken();
+        if (newToken) {
+          originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
+          return api(originalRequest);
+        }
+      } catch (e) {
+        localStorage.removeItem('token');
+        removeAuthToken();
+        window.location.href = '/login';
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
