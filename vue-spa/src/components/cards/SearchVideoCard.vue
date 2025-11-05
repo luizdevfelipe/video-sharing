59f43142524a8d82b17a60fef4f5464e3aeaca8d
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-11-05 17:36:26 -0300

    Componente totalmente responsivo

--- vue-spa/src/components/cards/SearchVideoCard.vue
+++ vue-spa/src/components/cards/SearchVideoCard.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
 const back_url = import.meta.env.VITE_APP_BACKEND
 
 const props = defineProps({
@@ -7,20 +8,37 @@ const props = defineProps({
     thumb_file: String,
 })
 
+const window_size = ref(window.innerWidth)
 const thumb_path = back_url + '/api/video/thumb/' + props.thumb_file
+
+const handleResize = () => {
+  window_size.value = window.innerWidth
+}
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize)
+})
 </script>
 
 <template>
-    <RouterLink :to="`/video/${id}`">
-        <div class="bg-gray-200 mx-auto p-2 my-5 rounded-lg sm:max-w-lg h-45 dark:bg-gray-800"
-            style="display: grid; grid-template: 100% / 55% 45%;">
-            <img :src="thumb_path" alt="" class="rounded-lg shadow-md dark:shadow-gray-800"
-                style="display: block; width: 100%; height: auto; max-height: 100%; object-fit: cover; align-self: center; justify-self: center;" />
-            <div class="p-2">
-                <p class="text-dark font-bold text-xl dark:text-white" :title="title">
-                    <!-- {{ title.length > 10 ? title.slice(0, 13) + '…' : title }} Adicionar verificação de tamanho de página -->
-                </p>
-            </div>
-        </div>
-    </RouterLink>
+  <RouterLink :to="`/video/${id}`" class="block">
+    <div
+      class="grid bg-gray-200 mx-auto my-5 p-2 rounded-lg md:max-w-xl lg:max-w-[62.5rem] dark:bg-gray-800 h-fit lg:grid-cols-[350px_auto] grid-cols-[50%_auto]"
+    >
+      <img
+        :src="thumb_path"
+        alt=""
+        class="rounded-lg shadow-md dark:shadow-gray-800 block w-full h-full max-h-full object-cover place-self-center"
+      />
+      <div class="p-2">
+        <p class="text-dark font-bold text-xl dark:text-white" :title="title">
+          {{ (title.length > 25 && window_size < 1024) || (title.length > 15 && window_size < 640) ? title.slice(0, 13) + '…' : title }}
+        </p>
+      </div>
+    </div>
+  </RouterLink>
 </template>
