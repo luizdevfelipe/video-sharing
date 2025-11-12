35c2acb8352aecfeae2821b8a15f927fad012af9
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-11-12 17:40:23 -0300

    Create SearchUsersCard.vue

---
+++ vue-spa/src/components/cards/SearchUsersCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+const back_url = import.meta.env.VITE_APP_BACKEND
+const props = defineProps({
+    name: String,
+    image: String,
+})
+
+const thumb_path = back_url + '/api/video/thumb/' + props.image;
+</script>
+
+<template>
+    <RouterLink :to="`/`">
+        <div class="">
+            <img :src="thumb_path" class="rounded-[50%] shadow-md dark:shadow-gray-800 block w-30 h-30 max-h-full object-cover place-self-center" alt="">
+            <p></p>
+        </div>
+    </RouterLink>
+</template>
