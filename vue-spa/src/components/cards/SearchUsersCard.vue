9954d66dc5d4932614596449776e492e7f0db0fb
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-11-13 19:10:00 -0300

    Limite no nome de perfil e tamanhos da imagem

--- vue-spa/src/components/cards/SearchUsersCard.vue
+++ vue-spa/src/components/cards/SearchUsersCard.vue
@@ -10,9 +10,9 @@ const thumb_path = back_url + '/api/video/thumb/' + props.image;
 
 <template>
     <RouterLink :to="`/`">
-        <div class="">
-            <img :src="thumb_path" class="rounded-[50%] shadow-md dark:shadow-gray-800 block w-30 h-30 max-h-full object-cover place-self-center" alt="">
-            <p></p>
+        <div class="text-center">
+            <img :src="thumb_path" class="rounded-[50%] shadow-md dark:shadow-gray-800 block w-25 h-25 md:w-50 md:h-50 max-h-full object-cover place-self-center" alt="">
+            <p> {{  name.length > 15 ? name.slice(0, 12) + '...' : name  }} </p>
         </div>
     </RouterLink>
 </template>
